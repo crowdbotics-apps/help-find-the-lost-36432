df0060194e71d87ee4a05a093a376cd8c73cbd71
Crowdbotics <team@crowdbotics.com>
2022-07-26 19:44:41 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/help_find_the_lost_36432/MainActivity.java
@@ -0,0 +1,15 @@
+package com.help_find_the_lost_36432;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "help_find_the_lost_36432";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "help_find_the_lost_36432",
+  url: "https://help_find_the_lost_36432.botics.co",
+  api: "https://help_find_the_lost_36432.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
